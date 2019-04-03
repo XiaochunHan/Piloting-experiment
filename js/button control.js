27da54085e8d8f02e0d353877c3144f89a954d0c
mollyz <zyzhang21@gmail.com>
2019-04-02 20:51:14 -0600

    save original

---
+++ js/button control.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+var rangeSlider = function(){
+    var slider = $('.range-slider'),
+        range = $('.range-slider input[type="range"]'),
+        value = $('.range-value');
+    slider.each(function(){
+        value.each(function(){
+            var value = $(this).prev().attr('value');
+            $(this).html(value);
+        });
+        range.on('input', function(){
+            $(this).next(value).html(this.value);
+        });
+    });
+};
+rangeSlider();
+});
+
+
+
+
+
+
+
+$('input[name="radio-btn"]').wrap('<div class="radio-btn"><i></i></div>');
+$(".radio-btn").on('click', function () {
+    var _this = $(this),
+        block = _this.parent().parent();
+    block.find('input:radio').attr('checked', false);
+    block.find(".radio-btn").removeClass('checkedRadio');
+    _this.addClass('checkedRadio');
+    _this.find('input:radio').attr('checked', true);
+});
+$('input[name="check-box"]').wrap('<div class="check-box"><i></i></div>');
+$.fn.toggleCheckbox = function () {
+    this.attr('checked', !this.attr('checked'));
+}
+$('.check-box').on('click', function () {
+    $(this).find(':checkbox').toggleCheckbox();
+    $(this).toggleClass('checkedBox');
+});
+
+
+
